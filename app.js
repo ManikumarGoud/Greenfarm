888ea866a5ec1da613f3a08fd5940cfd67c97a15
Somagani Mani Kumar <39871789+ManikumarGoud@users.noreply.github.com>
2020-09-29 16:22:51 +0530

    Deploy changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const expressValidator = require("express-validator");
 require("dotenv").config();
+const path = require('path');
 // import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -21,7 +22,7 @@ const app = express();
 mongoose
     .connect(process.env.DATABASE, {
         useNewUrlParser: true,
-        useCreateIndex: true
+        useCreateIndex: true, useUnifiedTopology: true 
     })
     .then(() => console.log("DB Connected"));
 
@@ -40,7 +41,21 @@ app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 const port = process.env.PORT || 8000;
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
